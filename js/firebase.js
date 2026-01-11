24b1d7f4b62a5878848cd02ef6768651331931cf
rihammlouk91-cyber <riham.mlouk91@gmail.com>
2026-01-11 17:55:35 +0400

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,12 @@
 // 1) Create Firebase project (web app) then paste config here
 const firebaseConfig = {
-  apiKey: "PASTE",
-  authDomain: "PASTE",
-  projectId: "PASTE",
-  storageBucket: "PASTE",
-  messagingSenderId: "PASTE",
-  appId: "PASTE",
+  apiKey: "AIzaSyC7jul8cNDMTu6w6wjKtHxFDy45h-q6ub8",
+  authDomain: "csr-tracker-2026.firebaseapp.com",
+  projectId: "csr-tracker-2026",
+  storageBucket: "csr-tracker-2026.firebasestorage.app",
+  messagingSenderId: "311719405004",
+  appId: "1:311719405004:web:5025163ab9517ef790e969",
+  measurementId: "G-N8YVNRW59L"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -36,3 +37,4 @@ function toDateOnlyISO(d) {
   const dd = String(x.getDate()).padStart(2, "0");
   return `${yyyy}-${mm}-${dd}`;
 }
+
